fcdbcde5f70a77a3174a23c3148ff9bd78af7b8c
tdekarz <tim.dekarz@gmx.net>
2019-01-25 01:14:07 +0100

    adds function to check internet connection

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const http = require('http')
+const https = require('https')
+
+const connectionCheck = (req, res) => {
+  https.get('https://www.google.com/search?q=omega', checkRes => {
+    if (checkRes.statusCode === 200) {
+      console.log('connection check successful')
+      res.end('internet reachable')
+    } else {
+      console.log(`connection check NOT successful => ${checkRes.statusCode}`)
+      res.end('internet NOT reachable')
+    }
+  })
+}
 
 const server = http.createServer((req, res) => {
   console.log(`${new Date().toUTCString()} ${req.method} ${req.url}`)
+
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-  res.end('αlpha')
+
+  //dispatch incoming request to handling extra action
+  if (req.url === '/connection-check') {
+    connectionCheck(req, res)
+  } else {
+    res.end('αlpha')
+  }
 })
 
 server.listen(8080)
